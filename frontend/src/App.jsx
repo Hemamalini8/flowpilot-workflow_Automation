0aa103d74bbe670bb9f4151c148752c92765f437
Hemamalini8 <hemamalinirajamanickam8@gmail.com>
2026-03-23 12:55:48 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-import WorkflowList from "./pages/workflowList";
+import WorkflowList from "./pages/WorkflowList";
 import WorkflowEditor from "./pages/workflowEditor";
 import StepManager from "./pages/StepManager";
 import RuleEditor from "./pages/ruleEditor";
@@ -16,10 +16,14 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/workflows" element={<WorkflowList />} />
+
         <Route path="/workflow-editor" element={<WorkflowEditor />} />
         <Route path="/workflow-editor/:id" element={<WorkflowEditor />} />
+
         <Route path="/steps/:workflowId" element={<StepManager />} />
+
         <Route path="/rules/:workflowId/:stepId" element={<RuleEditor />} />
+
         <Route path="/audit-log" element={<AuditLog />} />
       </Routes>
     </BrowserRouter>
